557af70f3b5a92325c406580b824643fb5b785f7
Shekow <sherifyousef209@gmail.com>
2024-12-13 18:34:58 +0200

    Getting started

---
+++ src/main/java/org/lms/entity/User.java
@@ -0,0 +1,47 @@
+package org.lms.entity;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private Integer role;
+
+    public User(Long id, String email, String password, Integer role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+}
