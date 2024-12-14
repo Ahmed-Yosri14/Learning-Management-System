1582301efb65cf882cb37a74fb4d780027c70db4
Ahmed-Yosri14 <147021297+Ahmed-Yosri14@users.noreply.github.com>
2024-12-14 19:09:10 +0200

    modified feedback

--- src/main/java/org/lms/entity/Feedback.java
+++ src/main/java/org/lms/entity/Feedback.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 @JsonTypeInfo(
@@ -22,28 +24,16 @@ public abstract class Feedback {
     @NotNull
     private long id;
 
+    @Getter
+    @Setter
     @NotNull
     private String feedback;
 
+    @Setter
+    @Getter
     @OneToOne(cascade = CascadeType.ALL)
     private Grade grade;
 
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
 
     public void setId(Long id) {
         this.id = id;
