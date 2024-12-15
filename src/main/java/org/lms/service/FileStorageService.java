4dbd733b34ec8cc229a7965788da2ca5ab41288c
Omar Abd El-Fatah <omarahmedgabrahmed111@gmail.com>
2024-12-15 19:08:29 +0200

    Course material added

---
+++ src/main/java/org/lms/service/FileStorageService.java
@@ -0,0 +1,28 @@
+package org.lms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class FileStorageService {
+    @Value("${upload.dir}")
+    private String uploadDir;
+
+    public String storeFile(MultipartFile file) throws IOException {
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        Path filePath = Paths.get(uploadDir, fileName);
+        Files.copy(file.getInputStream(), filePath);
+        return filePath.toString();
+    }
+}
